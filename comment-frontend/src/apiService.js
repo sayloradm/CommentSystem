884b7d93ec41f35ff218105c61e2b21f7db1d4d1
Adam Saylor <sayloradm@gmail.com>
2021-05-06 20:56:12 -0600

    Changed to run locally

--- comment-frontend/src/apiService.js
+++ comment-frontend/src/apiService.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 export default class ApiService {
   constructor() {
     this.http = axios.create({
-      baseURL: 'https://backend-309717.wm.r.appspot.com/api',
+      baseURL: 'http://localhost:5000/api',
+      // For production
+      // Also add "start": "ENV_PATH=./.env.production cross-env NODE_ENV=production node server.js",
+      // To backend package.json in scripts
+      //baseURL: 'https://backend-309717.wm.r.appspot.com/api',
       responseType: "json"
     })
   }
